48f04deae303eb47742ca7492311a92bb562c2ab
leejinseop <jjjp9966@gmail.com>
2023-04-02 22:11:10 +0900

    first commit

---
+++ src/test/java/com/ljs/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljs.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
